940df05cae2eb12cf3949fa3b3e27bd2a06baaf3
Juan Moreno <666.strigoi@gmail.com>
2020-05-27 13:35:52 -0500

    primer script

---
+++ installations.sh
@@ -0,0 +1,24 @@
+yay -S vscode
+yay -S visual-studio-code
+yay -S mailspring
+yay -Sy visual-studio-code-bin datagrip --noconfirm
+yay -Sy kdeenlive
+yay -Sy kdenlive
+yay -S i3-wm dunst i3lock i3status suckless-tools
+yay -S docker docker-compose
+yay -S playerctl
+yay -S unzip
+yay -S zip
+yay -Syu
+yay -Syu mailspring
+yay -Su ktorrent
+yay -S popcorntime
+yay -S npm
+yay -S nvm
+yay -S insomnia
+yay -S heroku
+yay -S yarn
+yay -S anydesk
+yay -S anydesk-bin
+yay -S genius
+yay -S ark
